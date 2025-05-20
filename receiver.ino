a5096902f2baa190254c9b418d73f13afcc3c341
Matt <Matt.simmons@gmail.com>
2025-05-19 22:06:03 -0400

    Create receiver.ino

---
+++ receiver.ino
@@ -0,0 +1,108 @@
+#include <Wire.h>  
+#include "LoRaWan_APP.h"
+#include "Arduino.h"
+#include "HT_SSD1306Wire.h"
+
+static SSD1306Wire  display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED); // addr , freq , i2c group , resolution , rst
+
+#define RF_FREQUENCY                                915000000 // Hz
+
+#define TX_OUTPUT_POWER                             14        // dBm
+
+#define LORA_BANDWIDTH                              0         // [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
+#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5, 2: 4/6, 3: 4/7, 4: 4/8]
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
+
+#define RX_TIMEOUT_VALUE                            1000
+#define BUFFER_SIZE                                 64 // Increased size for safety
+
+char txpacket[BUFFER_SIZE];
+char rxpacket[BUFFER_SIZE];
+
+static RadioEvents_t RadioEvents;
+
+int16_t txNumber;
+int16_t rssi,rxSize;
+
+bool lora_idle = true;
+
+void setup() {
+    Serial.begin(115200);
+    Mcu.begin(HELTEC_BOARD,SLOW_CLK_TPYE);
+    
+    txNumber=0;
+    rssi=0;
+    VextON();
+    delay(100);
+    display.init();
+
+    display.setFont(ArialMT_Plain_16);
+
+    display.drawString(0, 0, "FinishTime");
+    display.drawString(0, 20, "Waiting for Data");
+    display.display();
+  
+    RadioEvents.RxDone = OnRxDone;
+    Radio.Init( &RadioEvents );
+    Radio.SetChannel( RF_FREQUENCY );
+    Radio.SetRxConfig( MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                               LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                               LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                               0, true, 0, 0, LORA_IQ_INVERSION_ON, true );
+}
+
+void VextON(void)
+{
+  pinMode(Vext,OUTPUT);
+  digitalWrite(Vext, LOW);
+}
+
+void VextOFF(void) //Vext default OFF
+{
+  pinMode(Vext,OUTPUT);
+  digitalWrite(Vext, HIGH);
+}
+
+void loop()
+{
+  if(lora_idle)
+  {
+    lora_idle = false;
+    ///Serial.println("into RX mode");
+    Radio.Rx(0);
+  }
+  Radio.IrqProcess( );
+}
+
+void OnRxDone( uint8_t *payload, uint16_t size, int16_t packetRssi, int8_t snr )
+{
+    // Copy payload into rxpacket and null-terminate
+    memcpy(rxpacket, payload, size );
+    rxpacket[size] = '\0';
+
+    Radio.Sleep( );
+
+    Serial.printf(rxpacket);
+    Serial.println();
+
+    // Parse CSV string
+    char *carID = strtok(rxpacket, ",");
+    char *finishTime = strtok(NULL, ",");
+
+    display.clear();
+    display.setFont(ArialMT_Plain_24);
+
+    if (carID != NULL) {
+        display.drawString(0, 0, String(carID));
+    }
+    if (finishTime != NULL) {
+        display.drawString(0, 20, String(finishTime));
+    }
+    display.display();
+
+    lora_idle = true;
+}
