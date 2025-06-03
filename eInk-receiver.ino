9f4bc3c06850942f6b5eab6c04b76502da44e7c7
Matt <Matt.simmons@gmail.com>
2025-06-03 13:47:55 -0400

    Create eInk-receiver.ino

---
+++ eInk-receiver.ino
@@ -0,0 +1,86 @@
+#include <Wire.h>
+#include "LoRaWan_APP.h"
+#include "Arduino.h"
+#include "HT_DEPG0290BxS800FxX_BW.h"
+
+// E-Ink display setup (RST, DC, CS, BUSY, SCLK, MOSI, -1=no MISO, SPI freq)
+DEPG0290BxS800FxX_BW display(5, 4, 3, 6, 2, 1, -1, 6000000);
+
+// LoRa Parameters (raw LoRa mode)
+#define RF_FREQUENCY                915000000 // Hz
+#define TX_OUTPUT_POWER             14        // dBm
+#define LORA_BANDWIDTH              0         // 125 kHz
+#define LORA_SPREADING_FACTOR       7
+#define LORA_CODINGRATE             1         // 4/5
+#define LORA_PREAMBLE_LENGTH        8
+#define LORA_SYMBOL_TIMEOUT         0
+#define LORA_FIX_LENGTH_PAYLOAD_ON false
+#define LORA_IQ_INVERSION_ON        false
+
+#define RX_TIMEOUT_VALUE            1000
+#define BUFFER_SIZE                 80
+
+char rxpacket[BUFFER_SIZE];
+
+static RadioEvents_t RadioEvents;
+int16_t rssi;
+int8_t snr;
+bool lora_idle = true;
+
+void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi_, int8_t snr_) {
+  rssi = rssi_;
+  snr = snr_;
+  memcpy(rxpacket, payload, size);
+  rxpacket[size] = '\0';
+
+  Serial.printf("Received packet: %s\n", rxpacket);
+  Serial.printf("RSSI: %d, SNR: %d\n", rssi, snr);
+
+  display.clear();
+  display.drawString(0, 0, "RX:");
+  display.drawString(0, 20, rxpacket);
+
+  char info[32];
+  snprintf(info, sizeof(info), "RSSI: %d SNR: %d", rssi, snr);
+  display.drawString(0, 40, info);
+
+  display.display();
+}
+
+void setup() {
+
+  Mcu.begin(HELTEC_BOARD, SLOW_CLK_TPYE);
+  Serial.begin(115200);
+
+  VextON();
+  delay(100);
+
+  display.init();
+  display.clear();
+  display.setFont(ArialMT_Plain_24);
+  display.drawString(0, 0, "SuperLiveTimer Ready");
+  display.display();
+
+  RadioEvents.RxDone = OnRxDone;
+  Radio.Init(&RadioEvents);
+
+  Radio.SetChannel(RF_FREQUENCY);
+
+  Radio.SetRxConfig(MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                    LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                    LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                    0, true, 0, 0, LORA_IQ_INVERSION_ON, true);
+
+  Radio.Rx(0);
+  Serial.println("LoRa Receiver with E-Ink ready.");
+}
+
+void VextON(void)
+{
+  pinMode(18,OUTPUT);
+  digitalWrite(18, HIGH);
+}
+
+void loop() {
+  Radio.IrqProcess();
+}
